8e4500a25083d73dfeaa6554b69598af8a4910ff
aktush <sergey.aktush@ya.ru>
2024-07-09 11:11:54 +0300

    feature: input channel class

--- models/channel.py
+++ models/channel.py
@@ -1,14 +1,10 @@
 from abc import ABC, abstractmethod
+from typing import Literal
 
 
 class Channel(ABC):
-    def __init__(self, number: int) -> None:
-        if not isinstance(number, int):
-            raise ValueError("Number must be an integer.")
-        if not 0 <= number <= 63:
-            raise ValueError("Channel number must be between 0 and 64.")
-
-        self._number = number
+    def __init__(self) -> None:
+        self._number = 0
         self._name = ''
         self._mute = False
         self._gain = 0.0
@@ -73,3 +69,80 @@ class Channel(ABC):
     def __str__(self) -> str:
         return (f"Channel {self.number}: {self.name} - Mute: {self.mute}, "
                 f"Gain: {self.gain}, Linked: {self.link}, Level: {self.level}")
+
+
+class InputChannel(Channel):
+    def __init__(self, number: int, is_dante: bool = False, ) -> None:
+        super().__init__()
+        if not isinstance(number, int):
+            raise ValueError("Number must be an integer.")
+        if not 0 <= number <= 63:
+            raise ValueError("Channel number must be between 0 and 64.")
+
+        self._number = number
+        self._name = f'IN{number + 1}'
+        self._mute = False
+        self._phantom_power = False
+        self._gain = 0.0
+        self._link = False
+        self._level = -160.0
+        self._sensitivity: int = 0
+        self._source: Literal["input", "generator"] = "input"
+        self._isdante: bool = is_dante
+        self._phase: bool = False
+
+    @property
+    def source(self) -> str:
+        return self._source
+
+    @source.setter
+    def source(self, value: str) -> None:
+        if not isinstance(value, str):
+            raise ValueError("Source must be a string.")
+        if value not in ["input", "generator"]:
+            raise ValueError("Source can only be 'input' or 'generator'.")
+        self._source = value
+
+    @property
+    def isdante(self) -> bool:
+        return self._isdante
+
+    @property
+    def sensitivity(self) -> int:
+        if self._isdante:
+            raise AttributeError("Dante channels has no sensitivity settings")
+        return self._sensitivity
+
+    @sensitivity.setter
+    def sensitivity(self, value: int) -> None:
+        if self._isdante:
+            raise AttributeError("Dante channels has no sensitivity settings")
+        if not isinstance(value, int):
+            raise ValueError("Sensitivity must be an integer.")
+        if not 0 <= value <= 15:
+            raise ValueError("Sensitivity can only be between 0 and 15.")
+        self._sensitivity = value
+
+    @property
+    def phantom_power(self) -> bool:
+        if self._isdante:
+            raise AttributeError("Dante channels has no phantom power")
+        return self._phantom_power
+
+    @phantom_power.setter
+    def phantom_power(self, value: bool) -> None:
+        if self._isdante:
+            raise AttributeError("Dante channels has no phantom power")
+        if not isinstance(value, bool):
+            raise ValueError("Phantom power must be a boolean.")
+        self._phantom_power = value
+
+    @property
+    def phase(self) -> bool:
+        return self._phase
+
+    @phase.setter
+    def phase(self, value: bool) -> None:
+        if not isinstance(value, bool):
+            raise ValueError("Phase must be a boolean.")
+        self._phase = value
