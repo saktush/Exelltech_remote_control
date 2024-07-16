61a4fe04e4e5391bcae7ad3080f75a47b2d85481
s.blashkevich <s.a.blashkevich@gmail.com>
2024-07-16 14:27:36 +0300

    update: system functions ping added

--- models/system.py
+++ models/system.py
@@ -19,6 +19,18 @@ def hex_to_bytes(message: str) -> bytes:
     return bytes.fromhex(message)
 
 
+def ping_server(server: str, port: int, timeout=3):
+    """ping server"""
+    try:
+        socket.setdefaulttimeout(timeout)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((server, port))
+    except OSError as error:
+        return False
+    else:
+        s.close()
+        return True
+
 class UDP:
     """
     A class encapsulating UDP communication functionalities with configurable timeout and buffer size.
