a8becb60768192b8fc38410a7582921df2292786
s.blashkevich <s.a.blashkevich@gmail.com>
2024-07-16 15:22:29 +0300

    update: processor pull_channels logic

---
+++ examples/processor_live_pulling.py
@@ -0,0 +1,24 @@
+import os
+import platform
+
+from models.processor import ELTProcessor
+
+
+def clear():
+    if platform.system() == 'Windows':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+
+def main():
+    elt808d = ELTProcessor("192.168.3.110", 50000, 18, 18, 8)
+    elt808d.pull_channels()
+    for i in elt808d.input_channels:
+        print(i)
+
+
+if __name__ == '__main__':
+    while True:
+        clear()
+        main()
