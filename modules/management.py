40d1f2f80896f0f748b5c4e561bc368971edb65f
aktush <sergey.aktush@ya.ru>
2024-08-07 14:53:25 +0300

    update: pull matrix and push matrix methods added

--- modules/management.py
+++ modules/management.py
@@ -1,6 +1,6 @@
 import logging
 import time
-from typing import Optional, List
+from typing import Optional, List, Literal
 from config import LOCAL_IP, LOCAL_PORT
 from modules.api import ASCII as api
 from modules.abstract import Processor
@@ -91,3 +91,29 @@ class ChannelManager:
                 logging.error(f"Failed to parse gains values: {response_data}")
                 return None
         return None
+
+    @staticmethod
+    def pull_matrix_switches(proc: Processor) -> None:
+        for i, row in enumerate(proc.matrix.routes):
+            for k, cell in enumerate(row):
+                command = api.get.mixer.switch(i, k)
+                response_data = ChannelManager._send_and_parse(proc, command)
+                if response_data:
+                    try:
+                        value = bool(int(response_data))
+                    except ValueError:
+                        logging.error(f"Failed to parse matrix switch value: {response_data}")
+                        continue
+                    # set value
+                    proc.matrix.set_route(i, k, value)
+
+    @staticmethod
+    def push_matrix_switches(proc: Processor) -> Optional[True]:
+        for i, row in enumerate(proc.matrix.routes):
+            for k, switch in enumerate(row):
+                value: Literal[0, 1] = 1 if switch else 0
+                command = api.set.mixer.switch(i, k, value)
+                response_data = ChannelManager._send_and_parse(proc, command)
+                if response_data:
+                    return True
+
